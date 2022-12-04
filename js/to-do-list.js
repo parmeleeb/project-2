bf72f1b22d931fdb19b924c9d5987e1675539700
Brian P <parmeleeb@gmail.com>
2022-12-04 16:49:01 -0600

    Created JS code to add, delete, and transfer. No text edit yet.

--- js/to-do-list.js
+++ js/to-do-list.js
@@ -1,10 +1,124 @@
+const toDoList = ['clean the house', 'do the laundry', "check the quo'tations"];
+const completedList = ['wash the dishes', 'check the apo"strophes'];
 
-
-function test() {
-    console.log('test success');
+function deleteToDoItem(elementId) {
+    toDoList.splice(toDoList.indexOf(elementId), 1);
+    displayToDo();
 }
 
-function deleteItem() {
-    let item1 = document.getElementById("item1");
-    item1.remove();
+function deleteCompletedItem(elementId) {
+    completedList.splice(completedList.indexOf(elementId), 1);
+    console.log('deleting item?');
+    displayCompleted();
+}
+
+function displayToDoItem(elementId) {
+    const itemDiv = document.createElement('div');
+    const itemDescription = document.createElement('span');
+    const itemEdit = document.createElement('span');
+    const editButton = document.createElement('button');
+    const itemComplete = document.createElement('span');
+    const completeButton = document.createElement('button');
+    const itemDelete = document.createElement('span');
+    const deleteButton = document.createElement('button');
+
+    itemDiv.className = "item";
+    itemDiv.id = elementId;
+
+    itemDescription.className = "item-description";
+    itemDescription.innerText = elementId;
+
+    editButton.innerText = "Edit";
+    itemEdit.appendChild(editButton);
+    itemEdit.className = "item-edit";
+
+    completeButton.innerText = "Complete";
+    completeButton.onclick = () => completeItem(elementId);
+    itemComplete.appendChild(completeButton);
+    itemComplete.className = "item-complete";
+
+    deleteButton.innerText = "Delete";
+    deleteButton.onclick = () => deleteToDoItem(elementId);
+    itemDelete.appendChild(deleteButton);
+    itemDelete.className = "item-delete";
+
+    itemDiv.append(itemDescription, itemEdit, itemComplete, itemDelete);
+
+    const toDoList = document.getElementById("to-do-list");
+    toDoList.appendChild(itemDiv);
+}
+
+function displayCompletedItem(elementId) {
+    const itemDiv = document.createElement('div');
+    const itemDescription = document.createElement('span');
+    const itemEdit = document.createElement('span');
+    const editButton = document.createElement('button');
+    const itemIncomplete = document.createElement('span');
+    const incompleteButton = document.createElement('button');
+    const itemDelete = document.createElement('span');
+    const deleteButton = document.createElement('button');
+
+    itemDiv.className = "item";
+    itemDiv.id = elementId;
+
+    itemDescription.className = "item-description";
+    itemDescription.innerText = elementId;
+
+    editButton.innerText = "Edit";
+    itemEdit.appendChild(editButton);
+    itemEdit.className = "item-edit";
+
+    incompleteButton.innerText = "Incomplete";
+    incompleteButton.onclick = () => incompleteItem(elementId);
+    itemIncomplete.appendChild(incompleteButton);
+    itemIncomplete.className = "item-incomplete";
+
+    deleteButton.innerText = "Delete";
+    deleteButton.onclick = () => deleteCompletedItem(elementId);
+    itemDelete.appendChild(deleteButton);
+    itemDelete.className = "item-delete";
+
+    itemDiv.append(itemDescription, itemEdit, itemIncomplete, itemDelete);
+
+    const toDoList = document.getElementById("completed-list");
+    toDoList.appendChild(itemDiv);
+}
+
+function completeItem(elementId) {
+    addCompletedItem(elementId);
+    deleteToDoItem(elementId);
+}
+
+function incompleteItem(elementId) {
+    addToDoItem(elementId);
+    deleteCompletedItem(elementId);
+}
+
+function displayLists() {
+    displayToDo();
+    displayCompleted();
+}
+
+function displayToDo() {
+    document.getElementById("to-do-list").innerHTML = "";
+    for (let item of toDoList) {
+        displayToDoItem(item);
+    }
+}
+
+function displayCompleted() {
+    document.getElementById("completed-list").innerHTML = "";
+    for (let item of completedList) {
+        displayCompletedItem(item);
+    }
+}
+
+function addToDoItem(elementId) {
+    toDoList.push(elementId);
+    displayToDo();
+}
+
+function addCompletedItem(elementId) {
+    completedList.push(elementId);
+    displayCompleted();
 }
